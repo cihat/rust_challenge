f93c0632c759c88b4cead972217246a8fc4955d8
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2023-11-09 01:12:09 +0300

    wip

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,14 @@
-use std::io;
+use rand::Rng;
+use std::{io, cmp::Ordering};
+// use std::cmp::Ordering;
+// use std::io;
 
 fn main() {
   println!("Guess the number!");
+
+  let secret_number = rand::thread_rng().gen_range(1..=100);
+
+  println!("The secret number is: {secret_number}");
 
   println!("Please input your guess.");
 
@@ -11,5 +18,17 @@ fn main() {
     .read_line(&mut guess)
     .expect("Failed to read line.");
 
+  let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
   println!("You guees: {guess}");
+
+  match guess.cmp(&secret_number) {
+    Ordering::Less => println!("Too small!"),
+    Ordering::Greater => println!("Too big!"),
+    Ordering::Equal => println!("You win!"),
+  }
 }
+
+
+// I am here in the rust book 
+// https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html#allowing-multiple-guesses-with-looping
